e8c3ddce56011edce33dc5bf467eee8e350dc395
alduzarate <aldanazarate7@gmail.com>
2020-11-28 00:29:22 -0300

    eval2 ready

--- src/Eval2.hs
+++ src/Eval2.hs
@@ -35,19 +35,36 @@ instance Applicative StateError where
 
 -- Ejercicio 2.a: Dar una instancia de Monad para StateError:
 instance Monad StateError where
-  return x = undefined
-  m >>= f = undefined
+  -- instance Monad State where
+  --return x = State (\s -> (x :!: s))
+  --m >>= f = State (\s -> let (v :!: s') = runState m s in runState (f v) s')
+  return x = StateError (\s -> Right (x :!: s))
+  m >>= f  = StateError(\s -> runStateError m s >>= \(v :!: s') -> runStateError (f v) s')
+  -- Left error >>= f = StateError Left error
+  -- Right m >>= f = StateError (\s -> let (v :!: s') = runStateError m s in runStateError (f v) s')
 
 -- Ejercicio 2.b: Dar una instancia de MonadError para StateError:
--- COMPLETAR
+instance MonadError StateError where
+  throw e = StateError(\s -> Left e)
 
 -- Ejercicio 2.c: Dar una instancia de MonadState para StateError:
--- COMPLETAR
+instance MonadState StateError where
+  -- lookfor v = State (\s -> (lookfor' v s :!: s))
+  --   where lookfor' v s = fromJust $ M.lookup v s
+  -- update v i = State (\s -> (() :!: update' v i s)) where update' = M.insert
+  lookfor v = StateError(\s ->  case M.lookup v s of
+                                  Just n -> Right (n :!: s)
+                                  _      -> Left UndefVar)
+
+  update v i = StateError (\s -> Right (() :!: update' v i s)) where update' = M.insert
 
 -- Ejercicio 2.d: Implementar el evaluador utilizando la monada StateError.
 -- Evalua un programa en el estado nulo
 eval :: Comm -> Either Error Env
-eval = undefined
+eval p = case runStateError (stepCommStar p) initEnv of
+          Right(_ :!: s) -> Right s
+          Left x         -> Left x --Left Error
+
 
 -- Evalua multiples pasos de un comando, hasta alcanzar un Skip
 stepCommStar :: (MonadState m, MonadError m) => Comm -> m ()
@@ -56,9 +73,77 @@ stepCommStar c    = stepComm c >>= \c' -> stepCommStar c'
 
 -- Evalua un paso de un comando
 stepComm :: (MonadState m, MonadError m) => Comm -> m Comm
-stepComm = undefined
+stepComm Skip = return Skip
+stepComm (Let var e) =  do
+                          n <- evalExp e 
+                          update var n 
+                          return Skip
+stepComm (Seq Skip c2) = return c2                      
+stepComm (Seq c1 c2) = stepComm c1 >>= \c1' -> return (Seq c1' c2)
+stepComm (IfThenElse eb c1 c2) =  do
+                                    cond <- evalExp eb
+                                    if cond then return c1 else return c2
+stepComm w@(While eb c) = do
+                          cond <- evalExp eb
+                          if cond then return (Seq c w) else return Skip
 
 -- Evalua una expresion
 evalExp :: (MonadState m, MonadError m) => Exp a -> m a
-evalExp = undefined
+evalExp (Const e) = return e
+evalExp (Var e) = lookfor e
+evalExp (UMinus e) = evalExp e >>= \x -> return (-x)
+evalExp (Plus e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 + n2) 
+
+evalExp (Minus e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 - n2)
+evalExp (Times e1 e2) =   do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 * n2)
+evalExp (Div e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        if n2 == 0 then throw DivByZero else return (n1 `div` n2)
+
+evalExp BTrue = return True
+evalExp BFalse = return False
+
+evalExp (Lt e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 < n2)
+evalExp (Gt e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 > n2)
+evalExp (And e1 e2) = do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 && n2)
+evalExp (Or e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 || n2)
+evalExp (Not e) = evalExp e >>= \x -> return (not x)
+evalExp (Eq e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 == n2)
+evalExp (NEq e1 e2) =  do
+                        n1 <- evalExp e1
+                        n2 <- evalExp e2
+                        return (n1 /= n2)
+evalExp (EAssgn var e) =  do
+                            n1 <- evalExp e
+                            update var n1
+                            return n1     
+
+evalExp (ESeq e1 e2) = evalExp e1 >>= \_ -> evalExp e2 >>= \x -> return x
+
+
 
